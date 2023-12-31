dd4a5f11704ee2ec6bdf542da9b55e313775b6a6
Shantanu Sutar <shantanusutar17@gmail.com>
2023-12-31 23:21:53 +0530

    ui : Navbar - Logo

---
+++ airbnb-clone/app/components/Container.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const Container = ({ children }: Props) => {
+  return (
+    <div className=" max-w-[2520px] xl:px-20 md:px-10 sm:px-2 px-4">
+      {children}
+    </div>
+  );
+};
