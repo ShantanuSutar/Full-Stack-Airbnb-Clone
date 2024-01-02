cf9ca67387ec024efdcfd3dbce6abf3c6f005b62
Shantanu Sutar <shantanusutar17@gmail.com>
2024-01-03 00:31:44 +0530

    ui : Navbar -> UserMenu -> toggleOpen
    also, fixing hydration error ( when reload the page and click on the page instantly for many times )

--- airbnb-clone/app/components/Navbar/UserMenu.tsx
+++ airbnb-clone/app/components/Navbar/UserMenu.tsx
@@ -1,7 +1,14 @@
 "use client";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { useState } from "react";
+import MenuItem from "./MenuItem";
 export default function UserMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
     <div className=" relative">
       <div className=" flex flex-row items-center gap-3">
@@ -12,7 +19,9 @@ export default function UserMenu() {
           Airbnb Your Home
         </div>
         <div
-          onClick={() => {}}
+          onClick={() => {
+            toggleOpen();
+          }}
           className=" p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
         >
           <AiOutlineMenu />
@@ -21,6 +30,16 @@ export default function UserMenu() {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div className=" absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className=" flex flex-col cursor-pointer">
+            <>
+              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={() => {}} label="Sign up" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
