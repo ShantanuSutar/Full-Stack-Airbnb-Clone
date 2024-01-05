338aa63f7ad102f6934fef0d139a24ddbe59cf13
Shantanu Sutar <shantanusutar17@gmail.com>
2024-01-05 20:16:25 +0530

    ui : RegisterModal

--- airbnb-clone/app/components/Navbar/UserMenu.tsx
+++ airbnb-clone/app/components/Navbar/UserMenu.tsx
@@ -3,8 +3,11 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useState } from "react";
 import MenuItem from "./MenuItem";
+import RegisterModal from "../Modals/RegisterModal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 export default function UserMenu() {
   const [isOpen, setIsOpen] = useState(false);
+  const registerModal = useRegisterModal();
 
   const toggleOpen = () => {
     setIsOpen((prev) => !prev);
@@ -35,7 +38,7 @@ export default function UserMenu() {
           <div className=" flex flex-col cursor-pointer">
             <>
               <MenuItem onClick={() => {}} label="Login" />
-              <MenuItem onClick={() => {}} label="Sign up" />
+              <MenuItem onClick={registerModal.onOpen} label="Sign up" />
             </>
           </div>
         </div>
