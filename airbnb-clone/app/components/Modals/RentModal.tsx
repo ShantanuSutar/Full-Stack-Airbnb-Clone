5e547f545f3aacd59d304f5db9d1cf2af2b0c231
Shantanu Sutar <shantanusutar17@gmail.com>
2024-03-26 22:17:34 +0530

    ui : Rent Modal - map implementation

--- airbnb-clone/app/components/Modals/RentModal.tsx
+++ airbnb-clone/app/components/Modals/RentModal.tsx
@@ -8,6 +8,8 @@ import { categories } from "../Navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import Map from "../Map";
+import dynamic from "next/dynamic";
 
 enum STEPS {
   CATEGORY = 0,
@@ -45,6 +47,14 @@ const RentModal = () => {
 
   const category = watch("category");
   const location = watch("location");
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -111,6 +121,7 @@ const RentModal = () => {
           value={location}
           onChange={(value) => setCustomValue("location", value)}
         />
+        <Map center={location?.latlng} />
       </div>
     );
   }
