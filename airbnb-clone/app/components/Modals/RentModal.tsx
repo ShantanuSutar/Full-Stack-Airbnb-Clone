5aebd77e9d29bf0f6afcbd3635fcb69bbb2ca655
Shantanu Sutar <shantanusutar17@gmail.com>
2024-03-27 01:05:32 +0530

    ui : Rent Modal - image upload using cloudinary

--- airbnb-clone/app/components/Modals/RentModal.tsx
+++ airbnb-clone/app/components/Modals/RentModal.tsx
@@ -8,9 +8,9 @@ import { categories } from "../Navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
-import Map from "../Map";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -51,6 +51,7 @@ const RentModal = () => {
   const guestCount = watch("guestCount");
   const roomCount = watch("roomCount");
   const bathroomCount = watch("bathroomCount");
+  const imageSrc = watch("imageSrc");
 
   const Map = useMemo(
     () =>
@@ -166,6 +167,21 @@ const RentModal = () => {
           onChange={(value) => {
             setCustomValue("bathroomCount", value);
           }}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className=" flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like"
+        />
+        <ImageUpload
+          value={imageSrc}
+          onChange={(value) => setCustomValue("imageSrc", value)}
         />
       </div>
     );
