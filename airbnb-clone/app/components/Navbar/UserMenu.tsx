26a789f0ed6857b784c9e885bc4988a96e0a68cc
Shantanu Sutar <shantanusutar17@gmail.com>
2024-03-30 20:13:45 +0530

    ui : my trips page

--- airbnb-clone/app/components/Navbar/UserMenu.tsx
+++ airbnb-clone/app/components/Navbar/UserMenu.tsx
@@ -8,11 +8,13 @@ import useLoginModal from "@/app/hooks/useLoginModal";
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/app/types";
 import useRentModal from "@/app/hooks/useRentModal";
+import { useRouter } from "next/navigation";
 interface UserMenuProps {
   currentUser?: SafeUser | null;
 }
 
 export default function UserMenu({ currentUser }: UserMenuProps) {
+  const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
@@ -55,7 +57,12 @@ export default function UserMenu({ currentUser }: UserMenuProps) {
           <div className=" flex flex-col cursor-pointer">
             {currentUser ? (
               <>
-                <MenuItem onClick={() => {}} label="My trips" />
+                <MenuItem
+                  onClick={() => {
+                    router.push("/trips");
+                  }}
+                  label="My trips"
+                />
                 <MenuItem onClick={() => {}} label="My favorites" />
                 <MenuItem onClick={() => {}} label="My reservations" />
                 <MenuItem onClick={() => {}} label="My properties" />
