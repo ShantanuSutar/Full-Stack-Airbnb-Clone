d7e9466e66d2cec2889a74763e1d3d57f9b66e96
Shantanu Sutar <shantanusutar17@gmail.com>
2024-03-31 12:12:52 +0530

    ui & api : deleting your created listing

--- airbnb-clone/app/components/Navbar/UserMenu.tsx
+++ airbnb-clone/app/components/Navbar/UserMenu.tsx
@@ -75,7 +75,12 @@ export default function UserMenu({ currentUser }: UserMenuProps) {
                   }}
                   label="My reservations"
                 />
-                <MenuItem onClick={() => {}} label="My properties" />
+                <MenuItem
+                  onClick={() => {
+                    router.push("/properties");
+                  }}
+                  label="My properties"
+                />
                 <MenuItem onClick={rentModal.onOpen} label="Airbnb my home" />
                 <hr />
                 <MenuItem onClick={() => signOut()} label="Logout" />
