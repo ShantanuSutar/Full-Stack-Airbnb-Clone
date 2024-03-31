a7cf45995c1e0c6d94ec938c0b84a4b20f221e44
Shantanu Sutar <shantanusutar17@gmail.com>
2024-03-31 12:56:23 +0530

    ui & api : filtering the listings

--- airbnb-clone/app/layout.tsx
+++ airbnb-clone/app/layout.tsx
@@ -10,6 +10,7 @@ import LoginModal from "./components/Modals/LoginModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/Modals/RentModal";
+import SearchModal from "./components/Modals/SearchModal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <LoginModal />
           <RegisterModal />
