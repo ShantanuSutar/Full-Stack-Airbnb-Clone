0c22c242871bbdf275e2aa0e4632d55b758733b7
Shantanu Sutar <shantanusutar17@gmail.com>
2024-05-29 02:14:52 +0530

    deployment: error in getting server side props

--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -1,29 +1,18 @@
-import { useEffect, useState } from "react";
-import getCurrentUser from "./actions/getCurrentUser";
-import getListings, { IListingParams } from "./actions/getListings";
+import React from "react";
 import ClientOnly from "./components/ClientOnly";
 import { Container } from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import getListings, { IListingParams } from "./actions/getListings";
 import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
-interface HomeProps {
-  initialListings: Listing[];
+interface PageProps {
+  searchParams: IListingParams;
 }
 
-const Home = ({ initialListings }: HomeProps) => {
-  const [listings, setListings] = useState(initialListings);
-  const [currentUser, setCurrentUser] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      const listingsData = await getListings();
-      setListings(listingsData);
-      const currentUserData = await getCurrentUser();
-      setCurrentUser(currentUserData);
-    }
-
-    fetchData();
-  }, []);
+const Page = async ({ searchParams }: PageProps) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
@@ -37,17 +26,19 @@ const Home = ({ initialListings }: HomeProps) => {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
+          {listings.map((listing) => {
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
         </div>
       </Container>
     </ClientOnly>
   );
 };
 
-export default Home;
+export default Page;
