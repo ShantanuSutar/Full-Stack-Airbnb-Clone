d64621ac9696e2128cc1ed955d7666b9db419c57
Shantanu Sutar <shantanusutar17@gmail.com>
2024-05-29 01:54:22 +0530

    deployment: error in getting server side props

--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from "next";
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
@@ -6,13 +7,12 @@ import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
 interface HomeProps {
+  listings: any[];
+  currentUser: any;
   searchParams: IListingParams;
 }
 
-const Home = async ({ searchParams }: HomeProps) => {
-  const listings = await getListings(searchParams);
-  const currentUser = await getCurrentUser();
-
+const Home = ({ listings, currentUser, searchParams }: HomeProps) => {
   if (listings.length === 0) {
     return (
       <ClientOnly>
@@ -24,20 +24,32 @@ const Home = async ({ searchParams }: HomeProps) => {
   return (
     <ClientOnly>
       <Container>
-        <div className=" pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing) => {
-            return (
-              <ListingCard
-                currentUser={currentUser}
-                key={listing.id}
-                data={listing}
-              />
-            );
-          })}
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const searchParams = context.query as IListingParams;
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  return {
+    props: {
+      listings,
+      currentUser,
+      searchParams,
+    },
+  };
 };
 
 export default Home;
