df95356b045e2e6ad2f7b36da620c7d0dde87dbb
Shantanu Sutar <shantanusutar17@gmail.com>
2024-05-29 01:36:39 +0530

    deployment: error

--- airbnb-clone/app/api/reservations/route.ts
+++ airbnb-clone/app/api/reservations/route.ts
@@ -2,11 +2,11 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 
-export async function POST(request: Request) {
+export async function POST(request: Request): Promise<Response> {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) {
-    return NextResponse.error;
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   const body = await request.json();
@@ -14,24 +14,32 @@ export async function POST(request: Request) {
   const { listingId, startDate, endDate, totalPrice } = body;
 
   if (!listingId || !startDate || !endDate || !totalPrice) {
-    return NextResponse.error;
+    return NextResponse.json({ error: "Missing fields" }, { status: 400 });
   }
 
-  const listingAndReservation = await prisma.listing.update({
-    where: {
-      id: listingId,
-    },
-    data: {
-      reservations: {
-        create: {
-          userId: currentUser.id,
-          startDate,
-          endDate,
-          totalPrice,
+  try {
+    const listingAndReservation = await prisma.listing.update({
+      where: {
+        id: listingId,
+      },
+      data: {
+        reservations: {
+          create: {
+            userId: currentUser.id,
+            startDate,
+            endDate,
+            totalPrice,
+          },
         },
       },
-    },
-  });
+    });
 
-  return NextResponse.json(listingAndReservation);
+    return NextResponse.json(listingAndReservation);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
